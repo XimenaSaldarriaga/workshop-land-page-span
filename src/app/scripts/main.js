1696558377dc2712e7fe124daea6e1d810ef66da
ximena saldarriaga <xime-2208@hotmail.com>
2023-07-07 10:57:44 -0500

    create interactions with js

--- src/app/scripts/main.js
+++ src/app/scripts/main.js
@@ -1,1 +1,38 @@
 import "../styles/styles.scss";
+
+const menuCompany = document.querySelector('.menu__company')
+const companyArrow = document.querySelector('.header__companyarrow')
+const companyArrowUp = document.querySelector('.header__companyarrowup')
+const menuFeature = document.querySelector('.menu__features')
+const featureArrow = document.querySelector('.header__featuresarrow')
+const featureArrowUp = document.querySelector('.header__featuresarrowup')
+
+const toggleMenu = (menu, arrow, arrowUp) => {
+    menu.classList.toggle('show');
+    arrow.classList.toggle('hidden');
+    arrowUp.classList.toggle('hidden');
+  };
+  
+  const showMenuFeature = () => {
+    toggleMenu(menuFeature, featureArrow, featureArrowUp);
+  };
+  
+  const hideMenuFeature = () => {
+    toggleMenu(menuFeature, featureArrowUp, featureArrow);
+  };
+  
+  const showMenuCompany = () => {
+    toggleMenu(menuCompany, companyArrow, companyArrowUp);
+  };
+  
+  const hideMenuCompany = () => {
+    toggleMenu(menuCompany, companyArrowUp, companyArrow);
+  };
+  
+  featureArrow.addEventListener('click', showMenuFeature);
+  featureArrowUp.addEventListener('click', hideMenuFeature);
+  companyArrow.addEventListener('click', showMenuCompany);
+  companyArrowUp.addEventListener('click', hideMenuCompany);
+
+
+
